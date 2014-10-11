68e32cb8f09ad19e466710730c72bfafe5c79f1b
Pawel Maga <pawel.p.maga@gmail.com>
2014-10-11 18:03:30 +0200

    dodanie projektu z pierwszych zajec

---
+++ FizzBuzz/FizzBuzz/FizzBuzzTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzTests
+    {
+        [Fact]
+        public void FizzBuzz()
+        {
+            Assert.Equal(true, true);
+        }
+
+        [Fact]
+        public void Play_Return1ForArgument1_1()
+        {
+            var game = new Game();
+
+            var result = game.Play(1);
+
+            Assert.Equal("1", result);
+        }
+
+        [Fact]
+        public void Play_ReturnFizzForNumberDividedBy3_Fizz()
+        {
+            var game = new Game();
+
+            var result = game.Play(6);
+
+            Assert.Equal(result, "Fizz");
+        }
+
+        [Fact]
+        public void Play_ReturnBuzzForNumberDividedBy5_Fizz()
+        {
+            var game = new Game();
+
+            var result = game.Play(10);
+
+            Assert.Equal(result, "Buzz");
+        }
+
+        [Fact]
+        public void Play_ReturnFizzBuzzForNumberDividedBy3And5()
+        {
+            var game = new Game();
+
+            var result = game.Play(15);
+
+            Assert.Equal(result, "FizzBuzz");
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzz/Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class Game
+    {
+        public string Play(int p)
+        {
+            string result;
+            if (p%5 == 0 && p%3 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (p%3 == 0)
+            {
+                result = "Fizz";
+            }
+            else if (p%5 == 0)
+            {
+                result = "Buzz";
+            }
+            else
+            {
+                result = p.ToString();
+            }
+
+            return result;
+        }
+    }
+}
