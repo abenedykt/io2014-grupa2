5dbd38f2c7f33ab39f1a20508beffad26528dcbb
mostrowska <mostrowska91@gmail.com>
2014-10-11 18:08:03 +0200

    FizzBuzz

---
+++ FizzBuzz/FizzBuzz/FizzBuzz/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FizzBuzz
+{
+    public class Class1
+    {
+        [Fact]
+        public void FirstTest()
+        {
+            Assert.Equal(true, true);
+        }
+
+        [Fact]
+        public void Check_one_in_FizzBuzz()
+        {
+            Game game = new Game();
+            var result = game.Play(1);
+            Assert.Equal(result, "1");
+        }
+
+        [Fact]
+        public void Check_tree_in_FizzBuzz()
+        {
+            Game game = new Game();
+            var result = game.Play(3);            
+            Assert.Equal(result, "Fizz");
+        }
+
+        [Fact]
+        public void Check_five_in_FizzBuzz()
+        {
+            Game game = new Game();
+            var result = game.Play(5);
+            Assert.Equal(result, "Buzz");
+        }
+
+        [Fact]
+        public void Check_tree_and_five_in_FizzBuzz()
+        {
+            Game game = new Game();
+            var result = game.Play(15);
+            Assert.Equal(result, "FizzBuzz");
+        }
+    }
+
+}
---
+++ FizzBuzz/FizzBuzz/FizzBuzz/Game.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzz
+{
+    class Game
+    {
+        internal string Play(int p)
+        {
+            // sposob nr 1
+            string str = Console.ReadLine();
+            int result;
+
+            switch (str)
+            {                
+                case "FizzBuzz":
+                    result = p % 15;
+                    break;
+                
+                case "Fizz":
+                    result = p % 3;
+                    break;
+                
+                case "Buzz":
+                    result = p % 5;
+                    break;
+
+                default:
+                    result = 1;
+                    break;
+            }
+            
+            // sposob nr 2
+            if (p % 3 == 0 && p % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (p % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else if (p % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else
+            {
+                return 1.ToString();
+            }
+        }
+    }
+}
