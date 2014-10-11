f82be0f29dfa2d464fe32c8c2fbae5894e8b1474
Dominik Guz <dominik.guz11@gmail.com>
2014-10-11 17:38:41 +0200

    FizzBuzz

---
+++ FizzBuzz/FizzBuzz/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FizzBuzz
+{
+    
+    public class Class1
+    {
+
+        [Fact]
+        public void testMethod1()
+        {
+            Assert.Equal(true, true);
+        }
+
+        [Fact]
+        public void TestSimpleInput()
+        {
+            Game fizz = new Game();
+            Assert.Equal(fizz.play(1), 1);
+        }
+
+        [Fact]
+        public void TestComplexInput()
+        {
+            Game fizz = new Game();
+            Assert.Equal(fizz.play(3), 3);
+        }
+
+  
+
+
+    }
+}
---
+++ FizzBuzz/FizzBuzz/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzz
+{
+    public class Game
+    {
+        public int play(int num)
+        {
+            return num;
+        }
+
+    }
+
+}
