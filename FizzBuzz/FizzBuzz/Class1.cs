2c9b6ff93ad93ec5f211d34f163dc746af0b3aa0
ridali <ridali@gmail.com>
2014-10-25 11:46:02 +0200

    naprawa

---
+++ FizzBuzz/FizzBuzz/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FizzBuzz
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.Equal(true, true);
+        }
+
+        [Fact]
+        public void Jezeli_1_zwroc_1()
+        {
+            Game game = new Game();
+            var result = game.Play(1);
+
+            Assert.Equal(result, "1");
+        }
+
+        [Fact]
+        public void Jezeli_3_zwroc_Fizz()
+        {
+            Game game = new Game();
+            var result = game.Play(3);
+
+            Assert.Equal(result, "Fizz");
+        }
+
+        [Fact]
+        public void Jezeli_5_zwroc_Fazz()
+        {
+            Game game = new Game();
+            var result = game.Play(5);
+
+            Assert.Equal(result, "Bazz");
+        }
+
+        [Fact]
+        public void Jezeli_3i5_zwroc_FizzFazz()
+        {
+            Game game = new Game();
+            var result = game.Play(15);
+
+            Assert.Equal(result, "FizzBazz");
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzz/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzz
+{
+    class Game
+    {
+        internal string Play(int p)
+        {
+            if ((p % 3 == 0) && (p % 5 == 0))
+            {
+                return "FizzBazz";
+            }
+            if (p % 3 == 0)
+            {
+                return "Fizz";
+            }
+            if (p % 5 == 0)
+            {
+                return "Bazz";
+            }
+            return p.ToString();
+        }
+    }
+}
