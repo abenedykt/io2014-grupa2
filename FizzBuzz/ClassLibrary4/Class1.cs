a8955b8f804dea86fd3c76dc4d87294c7c53d0c3
Szymek <bonekpl@gmail.com>
2014-10-25 16:18:16 +0200

    Fizz Buzz

---
+++ FizzBuzz/ClassLibrary4/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ClassLibrary4
+{
+    public class Class1
+    {
+        [Fact]
+        public void test1()
+        {
+            Assert.Equal(true, true);
+        }
+
+        [Fact]
+        public void Sprawdzanie_dla_1()
+        {
+            Game game = new Game();
+            var result = game.Play("1");
+            Assert.Equal("1", result);
+        }
+
+        [Fact]
+        public void Sprawdzanie_dla_3()
+        {
+            Game game = new Game();
+            var result = game.Play("3");
+            Assert.Equal("fizz", result);
+        }
+
+        [Fact]
+        public void Sprawdzanie_dla_5()
+        {
+            Game game = new Game();
+            var result = game.Play("5");
+            Assert.Equal("buzz", result);
+        }
+
+    }
+}
---
+++ FizzBuzz/ClassLibrary4/Game.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary4
+{
+    class Game
+    {
+        internal string Play(string p)
+        {
+            if ((p!="3") && (p != "5"))
+            {
+                return p;                
+            }
+            else if (p=="3")
+            {
+                return "fizz";
+            }
+            else
+            {
+                return "buzz";
+            }
+
+        }
+    }
+}
