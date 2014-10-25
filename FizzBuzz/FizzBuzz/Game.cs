4e24e13b93b7765e0951e840b6ecb5b8da71bce5
Bartosz <itsgoodtoride@gmail.com>
2014-10-25 18:13:37 +0200

    asd

---
+++ FizzBuzz/FizzBuzz/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class Game
+    {
+        public string Play(int p)
+        {
+            string result = "";
+            if (p % 3 == 0) result = "Fizz";
+            if (p % 5 == 0) result += "Buzz";
+            if (result == "") result = p.ToString();
+
+            return result;
+        }
+    }
+}
