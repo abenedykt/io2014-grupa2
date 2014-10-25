e9a43929b9e2e38cc4e042dd5b6a124a334178d4
Jelonek <jelonek.renata@gmail.com>
2014-10-25 15:56:03 +0200

    gra

---
+++ fizzbuzz/ClassLibrary1/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        [Fact]
+        public void test1()
+        {
+            Assert.Equal(true, true);
+        }
+
+        [Fact]
+        public void daje_1_mam_1()
+        {
+            Game game = new Game();
+            var result = game.play("1");
+            Assert.Equal("1",result);
+        }
+
+        [Fact]
+        public void sprawdz_3()
+        {
+            Game game = new Game();
+            var result = game.play("3");
+            Assert.Equal("fizz",result);
+
+        }
+
+        [Fact]
+        public void sprawdz_5()
+        {
+            Game game = new Game();
+            var result = game.play("5");
+            Assert.Equal("buzz", result);
+        }
+    }
+}
---
+++ fizzbuzz/ClassLibrary1/Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    class Game
+    {
+        internal string play(string p)
+        {
+            if ((p != "3") && (p != "5"))
+	        {
+		         return p;
+	        }
+
+            else if (p == "3")
+            {
+                return "fizz";
+            }
+            
+            else 
+            {
+
+                return "buzz";
+            }
+
+            
+        }
+    }
+}
