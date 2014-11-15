4b9d78ab1e39d6f6c6f314ead4375b6d78c1b942
Waldiz <waldiz91@gmail.com>
2014-11-15 18:04:01 +0100

    NDepended

---
+++ PIO LAB/Kwadrat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIO_LAB
+{
+    class Kwadrat : Prostokat
+    {
+        public Kwadrat(int w) : base(w, w)
+        {
+            width = w;
+            height = w;
+        }
+    }
+}
---
+++ PIO LAB/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIO_LAB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kwadrat kwadrat = new Kwadrat(5);
+
+            Prostokat p = kwadrat;
+
+            p.height = 90;
+            p.width = 80;
+
+            //Prostokat prostokat = new Prostokat(5, 6);
+
+            Console.WriteLine(kwadrat.ObliczPole());
+            //Console.WriteLine(prostokat.ObliczPole());
+        }
+    }
+}
