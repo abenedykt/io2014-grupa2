30152b5617d2c6426bfc9de5c8efa71c3013c0f3
mostrowska <mostrowska91@gmail.com>
2014-11-15 18:10:08 +0100

    Dziedziczenie

---
+++ Dziedziczenie/Dziedziczenie/Dziedziczenie/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dziedziczenie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // var prostokat = new Prostokat(3.5, 4.5);
+            var kwadrat = new Kwadrat(3.2);
+
+            Prostokat p = kwadrat;
+            p.a = 5.2;
+            p.b = 4.3;
+           // Console.WriteLine(prostokat.Pole());
+            Console.WriteLine(kwadrat.Pole());
+
+        }
+    }
+}
---
+++ Dziedziczenie/Dziedziczenie/Dziedziczenie/Prostokat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dziedziczenie
+{
+    public class Prostokat
+    {
+        public double a { get; set; }
+        public double b { get; set; }
+
+        public Prostokat(double A, double B)
+        {
+            a = A;
+            b = B;
+        }
+
+        public virtual double Pole()
+        {
+            return a*b;
+        }
+    }
+}
