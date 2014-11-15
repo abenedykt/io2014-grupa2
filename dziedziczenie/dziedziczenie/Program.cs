59a5fa13ea3d2e7a2ac8b764ba5f4ce16e1e993b
Jelonek <jelonek.renata@gmail.com>
2014-11-15 17:49:56 +0100

    dziedziczenie

---
+++ dziedziczenie/dziedziczenie/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dziedziczenie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Kwadrat Kwadrat = new Kwadrat(5);
+        //Prostokat Prostokat = new Prostokat();
+
+        Prostokat p = Kwadrat;
+        p.height = 10;
+        p.width = 20;
+
+        Console.WriteLine(Kwadrat.ObliczPole());
+        }
+    }
+}
