9d98a6226dc9bf3432995de7cc1cbf96f30d01aa
jprze <jprze@windowslive.com>
2014-11-15 17:59:50 +0100

    zajecia_15112014_dziedziczenie

---
+++ PIO_Zajecia15112014/PIO_Zajecia15112014/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIO_Zajecia15112014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square ABCD = new Square(3.14);
+            Rectangle DCBA = new Rectangle(3, 4);
+
+            Console.WriteLine(ABCD.Area());
+            Console.WriteLine(DCBA.Area());
+            
+        }
+    }
+}
---
+++ PIO_Zajecia15112014/PIO_Zajecia15112014/Square.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIO_Zajecia15112014
+{
+    class Square: Rectangle
+    {
+
+
+        public Square(double SideLength)
+        {
+            a = SideLength;
+        }
+        
+        public override double Area()
+        {
+            return a * a;
+        }
+
+       
+    }
+}
