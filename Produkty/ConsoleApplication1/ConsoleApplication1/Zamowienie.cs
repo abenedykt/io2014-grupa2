8bcbaf8c24cc4588d5b635c4ab3f90bfc1a059e8
AS <shagatta@gmail.com>
2014-11-29 17:49:46 +0100

    NewProjekt Produkty

---
+++ Produkty/ConsoleApplication1/ConsoleApplication1/Zamowienie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Zamowienie 
+    {
+        IList<Produkt> Produkty { get; set; }
+        IList<Dostawa> Dostawa { get; set; }
+        IList<Rabat> Rabat { get; set; }
+    }
+}
