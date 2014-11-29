6349bbe9596e51dd72b7dfb2c3f1a8e0564c42a6
mostrowska <mostrowska91@gmail.com>
2014-11-29 17:43:33 +0100

    sklep internetowy

---
+++ Sklep/Sklep/Sklep/Produkt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sklep
+{
+    public class Produkt
+    {
+        
+        public Produkt ()
+	{
+        List<string> produkty = new List<string>();
+        produkty.Add("Komputer");
+        produkty.Add("Myszka");
+        produkty.Add("Klawiatura");
+        produkty.Add("Zasilacz";
+	}
+
+
+    }
+}
---
+++ Sklep/Sklep/Sklep/Zamowienia.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sklep
+{ //pojedyncze zamowienie
+    public class Zamowienia
+    {
+        public IList<Produkt> Produkty { get; set; }
+        public Klient Zamawiajacy { get; set; }
+        public Dostawa Dostawca { get; set; } 
+        public IList<Znizka> Zniżka { get; set; } //tutaj bedzie zmiana, bo sa rozne znizki
+    }
+}
