e27516330b38e100e45e150f61a772bfd150a097
Pawel Maga <pawel.p.maga@gmail.com>
2014-11-29 18:02:04 +0100

    zaj04

---
+++ Zamowienia/Zamowienia/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zamowienia
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var order = new Order();
+
+      order.Discounts.Add(new TenPercentDiscount());
+
+
+    }
+  }
+}
---
+++ Zamowienia/Zamowienia/TenPercentDiscount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zamowienia
+{
+  public class TenPercentDiscount : IDiscount
+  {
+    public decimal CalculatePrice(Order order)
+    {
+      var sum = order.Products.Sum(n => n.Price);
+
+      return sum - (sum*0.1m);
+    }
+  }
+}
