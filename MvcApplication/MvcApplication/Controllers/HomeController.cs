08a43bc14f753d711091b783a7104cddcf42ba48
Waldiz <waldiz91@gmail.com>
2015-01-24 18:05:19 +0100

    Walki

---
+++ MvcApplication/MvcApplication/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /FirstControler/
+
+        public ActionResult index()
+        {
+            return View("index", null, 12);
+        }
+
+    }
+}
