676394a6a7334f9a6f4cbd64c07d3e412b716672
Bartosz <itsgoodtoride@gmail.com>
2015-01-24 18:06:12 +0100

    gd

---
+++ PioCw9/PioCw9/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Autofac;
+using PioCw9.Logic;
+
+namespace PioCw9.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        private ContainerBuilder builder;
+
+        public HomeController()
+        {
+            builder = new ContainerBuilder();
+            builder.RegisterType<ActionResult>();
+            builder.Register(c => new CallLogger());
+        }
+
+       
+        public ActionResult Index()
+        {
+            return View("Index", null, DateTime.Now.Millisecond.ToString()); 
+        }
+
+    }
+}
---
+++ PioCw9/PioCw9/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PioCw9.MvcApplication" Language="C#" %>
