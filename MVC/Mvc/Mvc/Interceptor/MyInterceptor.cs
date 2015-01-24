c45c1f06fab16db599300363ec9471230c9d59a8
mostrowska <mostrowska91@gmail.com>
2015-01-24 18:15:41 +0100

    zajęcia 24.01.2015

---
+++ MVC/Mvc/Mvc/Interceptor/MyInterceptor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Autofac;
+using Castle.DynamicProxy;
+using System.IO;
+using Autofac.Extras.DynamicProxy2;
+
+namespace Mvc.Interceptor
+{
+    public class MyInterceptor : IInterceptor
+    {
+        TextWriter _output;
+        public MyInterceptor(TextWriter output)
+        {
+            _output = output;
+        }
+
+        public void Intercept(IInterceptor invocation)
+        {
+            
+
+        }
+    }
+}
