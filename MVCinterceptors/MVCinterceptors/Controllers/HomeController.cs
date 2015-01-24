7d2d5ac6ebb92abb160340c3d062854caac22789
ridali <ridali@gmail.com>
2015-01-24 18:11:47 +0100

    Interceptor

---
+++ MVCinterceptors/MVCinterceptors/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCinterceptors.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var date = (DateTime.Now.Millisecond).ToString();
+            return View("Index", null, date);
+        }
+
+    }
+}
---
+++ MVCinterceptors/MVCinterceptors/Interceptors/Interceptor.cs
@@ -0,0 +1,30 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace MVCinterceptors.Interceptors
+{
+    public class Interceptor : IInterceptor
+    {
+        TextWriter _output;
+
+        public Interceptor(TextWriter output)
+        {
+            _output = output;
+        }
+
+        public void Intercept(IInvocation invocation)
+        {
+            _output.Write("Calling method {0} with parameters {1}... ",
+              invocation.Method.Name,
+              string.Join(", ", invocation.Arguments.Select(a => (a ?? "").ToString()).ToArray()));
+
+            invocation.Proceed();
+
+            _output.WriteLine("Done: result was {0}.", invocation.ReturnValue);
+        }
+    }
+}
