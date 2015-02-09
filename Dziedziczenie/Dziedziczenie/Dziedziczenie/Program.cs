3ac40c4c6e9c8cef014afe1ff3c36831991010ab
mostrowska <mostrowska91@gmail.com>
2015-02-09 17:59:03 +0100

    modyfikacja

--- Dziedziczenie/Dziedziczenie/Dziedziczenie/Program.cs
+++ Dziedziczenie/Dziedziczenie/Dziedziczenie/Program.cs
@@ -10,13 +10,12 @@ namespace Dziedziczenie
     {
         static void Main(string[] args)
         {
-           // var prostokat = new Prostokat(3.5, 4.5);
             var kwadrat = new Kwadrat(3.2);
 
             Prostokat p = kwadrat;
             p.a = 5.2;
             p.b = 4.3;
-           // Console.WriteLine(prostokat.Pole());
+
             Console.WriteLine(kwadrat.Pole());
 
         }
