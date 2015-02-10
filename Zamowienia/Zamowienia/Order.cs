318d7844930803efc411f85829e031c6dbc0e628
Kamil Heczko <hozinio@gmail.com>
2015-02-10 11:03:19 +0100

    zalegle

---
+++ Zamowienia/Zamowienia/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zamowienia
+{
+  public class Order
+  {
+    public IList<IProduct> Products { get; set; }
+    public IList<IDiscount> Discounts { get; set; }
+    public Delivery Delivery { get; set; }
+    public Client Client { get; set; }
+
+    public decimal GetFinalPrice()
+    {
+      
+    }
+  }
+}
